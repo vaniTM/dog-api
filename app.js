c239f84694369f4ecc396d9b607a1a665f6228a0
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2021-03-09 17:48:17 +0100

    some minor errors
    - added a lot of ";"
    - app.js, 8:  missing () for json-method
    - index.html, 12: attribute should be id, not class

--- app.js
+++ app.js
@@ -1,78 +1,84 @@
-let timer
-let deleteFirstPhotoDelay
+let timer;
+let deleteFirstPhotoDelay;
 
 // fetch the data and make a error message
-async function start(){
-    try {
-    const response = await fetch("https://dog.ceo/api/breeds/list/all")
-    const data = await response.json
-    createBreedList(data.message)
-    }
-    catch (e) {
-        console.log("There was a problem fetching the breed list.")
-    }
+async function start() {
+  try {
+    const response = await fetch("https://dog.ceo/api/breeds/list/all");
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+    createBreedList(data.message);
+  } catch (e) {
+    console.log(e);
+  }
 }
-//start the start function 
-start()
+//start the start function
+start();
 
 //Take the Data and create the Drop-Down menu
 function createBreedList(breedList) {
-    document.getElementById("breed").innerHTML = `
+  document.getElementById("breed").innerHTML = `
     <select onchange="loadByBreed(this.value)">
           <option>Choose a dog breed</option>
           //makes an Array
-          ${Object.keys(breedList).map(function (breed) {
-            return `<option>${breed}</option>`
-          }).join('')}
+          ${Object.keys(breedList)
+            .map(function(breed) {
+              return `<option>${breed}</option>`;
+            })
+            .join("")}
         </select>
-    `
-  } 
+    `;
+}
 //fetsch the images
 async function loadByBreed(breed) {
-if (breed != "Choose a dog breed"){
-    const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`)
-    const data = await response.json()
-    createSlideshow(data.message)
-}
+  if (breed != "Choose a dog breed") {
+    const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`);
+    const data = await response.json();
+    createSlideshow(data.message);
+  }
 }
 //put the images in a slideshow and change the image every 3 sec.
 function createSlideshow(images) {
-    let currentPosition = 0 
-    clearInterval(timer)
-    clearTimeout(deleteFirstPhotoDelay)
+  let currentPosition = 0;
+  clearInterval(timer);
+  clearTimeout(deleteFirstPhotoDelay);
 
-    //if there are more then 1 images stwich through the images and loop it
-    if (images.length < 1) {
-        document.getElementById("slideshow").innerHTML = `
+  //if there are more then 1 images stwich through the images and loop it
+  if (images.length < 1) {
+    document.getElementById("slideshow").innerHTML = `
         <div class="slide" style="background-image: url('${images[0]}')"></div>
         <div class="slide" style="background-image: url('${images[1]}')"></div>
-        `
-        currentPosition += 2
-        if (images.length == 2) {
-            currentPosition = 0
-        }
-        timer = setInterval(nextSlide, 3000)
+        `;
+    currentPosition += 2;
+    if (images.length == 2) {
+      currentPosition = 0;
     }
-    //else keep the only one image and do nothing
-    else {
-        document.getElementById("slideshow").innerHTML = `
+    timer = setInterval(nextSlide, 3000);
+  }
+  //else keep the only one image and do nothing
+  else {
+    document.getElementById("slideshow").innerHTML = `
         <div class="slide" style="background-image: url('${images[0]}')"></div>
         <div class="slide"></div>
-        `
+        `;
+  }
+  //change the image all 3 sec. and if you change the breed clean the timer and the images lenght
+  setInterval(nextSlide, 3000);
+  function nextSlide() {
+    document
+      .getElementById("slideshow")
+      .insertAdjacentHTML(
+        "beforeend",
+        `<div class="slide" style="background-image: url('${images[currentPosition]}')"></div>`
+      );
+    deleteFirstPhotoDelay = setTimeout(function() {
+      document.querySelector(".slide").remove();
+    }, 1000);
+    if (currentPosition + 1 >= images.length) {
+      currentPosition = 0;
+    } else {
+      currentPosition++;
     }
-//change the image all 3 sec. and if you change the breed clean the timer and the images lenght
-setInterval(nextSlide, 3000)
-function nextSlide() {
-    document.getElementById("slideshow").insertAdjacentHTML("beforeend",`<div class="slide" style="background-image: url('${images[currentPosition]}')"></div>`)
-    deleteFirstPhotoDelay = setTimeout(function () {
-        document.querySelector(".slide").remove()
-    }, 1000)
-    if (currentPosition +1 >= images.length) {
-        currentPosition = 0
-    } 
-    else
-    {
-        currentPosition ++
-    }
-}
+  }
 }
